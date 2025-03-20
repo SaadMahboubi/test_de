0bd85f8c25136c5c24709daa4bd70247db5e03d7
SaadMahboubi <71195241+SaadMahboubi@users.noreply.github.com>
2025-03-20 15:27:42 +0100

    Ajout requete 2
    Ajout du résultat de la requête 2

---
+++ test_sql/requete_2.sql
@@ -0,0 +1,9 @@
+SELECT
+    t.client_id as client_id,
+    SUM(CASE WHEN p.product_type = 'MEUBLE' THEN t.prod_price * t.prod_qty ELSE 0 END) AS ventes_meuble,
+    SUM(CASE WHEN p.product_type = 'DECO' THEN t.prod_price * t.prod_qty ELSE 0 END) AS ventes_deco
+FROM TRANSACTIONS t
+INNER JOIN PRODUCT_NOMENCLATURE p ON t.prod_id = p.product_id
+WHERE t.date BETWEEN '2019-01-01' AND '2019-12-31'
+GROUP BY t.client_id
+ORDER BY t.client_id;
